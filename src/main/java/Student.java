12617763fd82f655ce01307f7ef9cc671b1dc93b
Rayzog <w.fedorov.ivan@gmail.com>
2020-07-30 23:15:33 +0300

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int fullAge;
+    private boolean experience;
+    private String learningObjective;
+    private String group;
+    private int countCompletedTask;
+    private int countCompletedModules;
+    
+    private void doTask(){}
+
+    private void askQuestion(){}
+
+    private void passTheModule(){}
+
+    private void writeToCurator(){}
+}
